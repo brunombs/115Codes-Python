072dd2fea6e617c437d576968469d4c3ae1132a5
brunombs <99062708+brunombs@users.noreply.github.com>
2022-07-17 23:30:42 -0300

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+dobro = numero*2
+triplo = numero*3
+raizquadrada = numero**(1/2)
+print('O número selecionado foi {}, o dobro desse número é {}, o triplo é {} e a raiz quadrada é {}'.format(numero, dobro, triplo, raizquadrada))
---
+++ 08.py
@@ -0,0 +1,4 @@
+metros = int(input('Qual o tamanho em metros? '))
+centimetro = metros*100
+milimetro = metros*1000
+print('Essa metragem em centimetros é equivalente a {} e em milimetros é equivalente a {}'.format(centimetro, milimetro))
