21c5685c22bc77d53375b507fdb1d1bacb111524
brunombs <99062708+brunombs@users.noreply.github.com>
2022-07-18 05:20:24 -0300

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número: '))
+print('{} * {} = {} \n{} * {} = {} \n{} * {} = {} \n{} * {} = {}'.format(numero, 1, numero, numero, 2, numero*2, numero, 3, numero*3, numero, 4, numero*4))
+print('{} * {} = {} \n{} * {} = {} \n{} * {} = {} \n{} * {} = {}'.format(numero, 5, numero*5, numero, 6, numero*6, numero, 7, numero*7, numero, 8, numero*8))
+print('{} * {} = {} \n{} * {} = {}'.format(numero, 9, numero*9, numero, 10, numero*10))
