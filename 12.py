776b5a01790d36593d56404b4b24706830b60ab7
brunombs <99062708+brunombs@users.noreply.github.com>
2022-07-19 01:42:27 -0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,2 @@
+precoproduto = int(input('Digite o valor do produto: '))
+print('Esse produto está com 5% de desconto, então custa {} reais'.format(precoproduto * 0.95))
---
+++ 14.py
@@ -0,0 +1,3 @@
+celsius = float(input('Qual é a temperatura em graus celsius?: '))
+fahrenheit = (celsius * 9/5) + 32
+print('A temperatura {}ºC corresponde a {}ºF'.format(celsius, fahrenheit))
