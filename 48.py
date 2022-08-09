ecac69630c504c6e897ac01660b5eb488d806c42
brunombs <99062708+brunombs@users.noreply.github.com>
2022-08-08 23:49:53 -0300

    Add files via upload

--- 48.py
+++ 48.py
@@ -1,4 +1,7 @@
+s = 0
 for c in range(1, 500, 2):
     if (c % 3) == 0:
         print(c)
+        s = s + c
 print('Fim')
+print('A soma de todos os valores é:', s)
--- 50.py
+++ 50.py
@@ -1,11 +1,15 @@
 p = 0
 i = 0
+np = 0
+ni = 0
 for c in range (0, 6):
     numero = int(input('Digite um número inteiro: '))
     if numero % 2 == 0:
         p = p + numero
+        np += 1
     elif numero % 2 == 1:
         i = i + numero
-print('A soma dos valores pares é {}'.format(p))
-print('A soma dos valores ímpares é {}'.format(i))
+        ni += 1
+print('Você informou {} números pares e a soma desses valores é {}'.format(np, p))
+print('Você informou {} números ímpares e a soma desses valores é {}'.format(ni, i))
 print('Fim')
