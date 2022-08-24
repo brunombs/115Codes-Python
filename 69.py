ac62cadd9f9a4153589b929123b28ff664845c59
brunombs <99062708+brunombs@users.noreply.github.com>
2022-08-23 23:46:51 -0300

    Add files via upload

---
+++ 69.py
@@ -0,0 +1,36 @@
+homem = 0
+mulher = 0
+pessoasdezoito = 0
+mulheresmenosdevinte = 0
+idade = int(input('Digite a sua idade: '))
+if idade >= 18:
+    pessoasdezoito += 1
+sexo = str(input('Digite o seu sexo: [M/F]: ')).upper()
+if sexo == ('M'):
+    homem += 1
+elif sexo == ('F') and idade <= 20:
+    mulheresmenosdevinte += 1
+while sexo != ('M') and sexo != ('F'):
+    sexo = str(input('Opção inválida, tente novamente! Digite "M" para masculino e "F" para feminino: ')).upper()
+    if sexo == ('M'):
+        homem += 1
+    elif sexo == ('F') and idade <= 20:
+        mulheresmenosdevinte += 1
+continuar = str(input('Você deseja continuar? [S/N]: ')).upper()
+while continuar != ('S') and continuar != ('N'):
+    continuar = str(input('Opção inválida, tente novamente! Digite "S" para sim e "N" para não: ')).upper()
+while continuar == ('S'):
+    idade = int(input('Digite a sua idade: '))
+    if idade >= 18:
+        pessoasdezoito += 1
+    sexo = str(input('Digite o seu sexo: [M/F]: ')).upper()
+    if sexo == ('M'):
+        homem += 1
+    elif sexo == ('F') and idade <= 20:
+        mulheresmenosdevinte += 1
+    continuar = str(input('Você deseja continuar? [S/N]: ')).upper()
+    if continuar == ('N'):
+        break
+print(f'{pessoasdezoito} pessoa(s) tem dezoito anos ou mais.')
+print(f'Foram cadastrados {homem} homem(s)!')
+print(f'{mulheresmenosdevinte} mulher(es) tem menos de vinte anos.')
