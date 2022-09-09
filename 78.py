bb5d553397d6d7affa1076a19cf830c8476ccb2a
brunombs <99062708+brunombs@users.noreply.github.com>
2022-09-09 14:32:48 -0300

    Add files via upload

--- 78.py
+++ 78.py
@@ -1,15 +1,26 @@
 lista = list()
-maior = 0
 menor = 0
-for cont in range (0, 5):
-    lista.append(int(input(f'Digite um número inteiro para a posição {cont}: ')))
-    if cont == 0:
-        maior = menor = lista[cont]
+maior = 0
+for contador in range (0, 5):
+    lista.append(int(input(f'Digite um número nas posições {contador}: ')))
+    if contador == 0:
+        maior = menor = lista[contador]
     else:
-        if lista[cont] > maior:
-            maior = lista[cont]
-        if lista[cont] < menor:
-            menor = lista[cont]
-for c, v in enumerate (lista):
-    print(f'Na posição {c} está o valor {v}')
-print(f'O menor valor da lista é {menor} e o maior é {maior}')
+        if lista[contador] > maior:
+            maior = lista[contador]
+        if lista[contador] < menor:
+            menor = lista[contador]
+print('='*60)
+print(f'Você digitou os valores: {lista}')
+print('='*60)
+print(f'O maior número digitado foi: {maior} nas posições: ', end='')
+for contador, número in enumerate(lista):
+    if maior == número:
+        print(f'{contador}... ', end='')
+print()
+print(f'O menor número digitado foi: {menor} nas posições: ', end='')
+for contador, número in enumerate(lista):
+    if menor == número:
+        print(f'{contador}... ', end='')
+print()
+print('='*60)
