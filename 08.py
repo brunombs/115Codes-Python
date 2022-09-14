dd1012c484bc0a9b7557514e6e6a334c4e788277
brunombs <99062708+brunombs@users.noreply.github.com>
2022-09-14 08:29:37 -0300

    Rename 08.py to 08 - Conversor de medidas.py
    "Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros."

--- 08.py
+++
@@ -1,4 +0,0 @@
-metros = int(input('Qual o tamanho em metros? '))
-centimetro = metros*100
-milimetro = metros*1000
-print('Essa metragem em centimetros é equivalente a {} e em milimetros é equivalente a {}'.format(centimetro, milimetro))
