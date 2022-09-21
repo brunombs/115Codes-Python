37bb406099f4fee190f91c2ef74c8724e6bd7c7d
brunombs <99062708+brunombs@users.noreply.github.com>
2022-09-21 20:02:55 -0300

    Update 85 - Listas com pares e ímpares.py

--- 85 - Listas com pares e ímpares.py
+++ 85 - Listas com pares e ímpares.py
@@ -1,12 +1,10 @@
-números = list()
-pares = list()
-ímpares = list()
-for cont in range(0, 7):
-    números.append(int(input('Digite um número: ')))
-    if números[cont] % 2 == 0:
-        pares.append(números[cont])
-    if números[cont] % 2 == 1:
-        ímpares.append(números[cont])
-print(números)
-print(f'Os valores pares da lista são: {sorted(pares)}')
-print(f'Os valores ímpares da lista são: {sorted(ímpares)}')
+números = [[], []]
+for cont in range(1, 8):
+    número = int(input(f'Digite o {cont}ª número inteiro: '))
+    if número % 2 == 0:
+        números[1].append(número)
+    if número % 2 == 1:
+        números[0].append(número)
+print(f'A lista com todos os números digitados é: \033[31m{números}\033[m')
+print(f'A lista em ordem crescente dos números pares é: \033[32m{sorted(números[1])}\033[m')
+print(f'A lista em ordem crescente dos números ímpares é: \033[33m{sorted(números[0])}\033[m')
