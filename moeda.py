3a11be2dbd4237373b443b84b35f6f306c3cd7d9
brunombs <99062708+brunombs@users.noreply.github.com>
2022-11-22 16:59:20 -0300

    Add files via upload

---
+++ moeda.py
@@ -0,0 +1,15 @@
+def aumentar(num):
+    aumentado = num * 1.10
+    return aumentado
+
+def diminuir(num):
+    diminuido = num * 0.90
+    return diminuido
+
+def dobro(num):
+    dobro = num * 2
+    return dobro
+
+def metade(num):
+    metade = num / 2
+    return metade
