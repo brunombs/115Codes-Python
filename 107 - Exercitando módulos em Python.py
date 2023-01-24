dc29c2550f151a4182eacfcf1d1a14ca49047098
brunombs <99062708+brunombs@users.noreply.github.com>
2023-01-24 03:06:33 +0000

    Delete 107 - Exercitando módulos em Python.py

--- 107 - Exercitando módulos em Python.py
+++
@@ -1,9 +0,0 @@
-import moeda
-numero = float(input('Digite o preço em R$: '))
-print(f'''
-O valor {numero}:
-+ 10% = {moeda.aumentar(numero, 10):.3f}
-- 10% = {moeda.diminuir(numero, 10):.3f}
-* 2 = {moeda.dobro(numero):.3f}
-/ 2 = {moeda.metade(numero):.3f}
-''')
