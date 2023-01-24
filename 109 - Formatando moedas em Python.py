b9a6c105590e00a4bd5f9c352770091223a0e3c5
brunombs <99062708+brunombs@users.noreply.github.com>
2023-01-24 03:06:38 +0000

    Delete 109 - Formatando moedas em Python.py

--- 109 - Formatando moedas em Python.py
+++
@@ -1,9 +0,0 @@
-import moeda
-numero = float(input('Digite o preço em R$: '))
-print(f'''
-O valor {numero}:
-+ 10% = {moeda.aumentar(numero, 20)}
-- 10% = {moeda.diminuir(numero, 10)}
-* 2 = {moeda.dobro(numero, True)}
-/ 2 = {moeda.metade(numero)}
-''')
